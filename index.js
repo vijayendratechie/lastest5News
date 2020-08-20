73d99b0ee53001fe8a7e2fc1c4652e8fed29513c
vijayendra <vijayendra@automate.io>
2020-08-20 21:37:55 +0530

    UPDATE: chosse dynamic port

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ app.get("/latestNews",function(req,res){
 
 })
 
-app.listen(port = 3000, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const port=process.env.PORT || 3000
+app.listen(port,function()
+{
+	console.log(`Example app listening at http://localhost:${port}`)
+});
